9f789d29daac13d1c199ad295be40988c028026f
thatTJguy <117551106+thatTJguy@users.noreply.github.com>
2025-06-11 16:57:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,14 +28,14 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_sercurity_group" "blog" {
+resource "aws_security_group" "blog" {
   name        = "blog"
   description = "allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in"
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -45,7 +45,7 @@ resource "aws_security_group_rule" "blog_http_in"
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_https_in"
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -55,7 +55,7 @@ resource "aws_security_group_rule" "blog_https_in"
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_everything_out"
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "engress"
   from_port   = 0
   to_port     = 0
